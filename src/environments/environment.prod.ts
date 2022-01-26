040992847e3074fbcba759d60e4b8bfd20ba2c35
kanondrago <ronaldchavezr@gmail.com>
2022-01-25 22:08:37 -0500

    textos, media queries y audio mar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: true,
-  url_base_backend: 'http://reserva.buenavistamancora.com/api',
-  url_root_backend: 'http://reserva.buenavistamancora.com',
+  // url_base_backend: 'http://reserva.buenavistamancora.com/api',
+  // url_root_backend: 'http://reserva.buenavistamancora.com',
+  // url_root_frontend: 'http://localhost:4200',
+  url_base_backend: 'http://localhost:8000/api',
+  url_root_backend: 'http://localhost:8000',
   url_root_frontend: 'http://localhost:4200',
-  key_acceso_backend: 'lCmLyv2S.Sdl8zkbtI9RJ4v37xJIENTdQUvbAAebY'
+  key_acceso_backend: 'mpfhKCBF.JQ08Bw1ctfX3OmdJ6vEgQXR93IJ4f5qO'
 };
