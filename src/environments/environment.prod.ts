0c996c1650dd62fd62129e89ff57511064253d44
kanondrago <ronaldchavezr@gmail.com>
2022-02-04 14:51:27 -0500

    Cambios 14:51 04/02/22 Spanish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   url_base_backend: 'http://reserva.buenavistamancora.com/api',
   url_root_backend: 'http://reserva.buenavistamancora.com',
   url_root_frontend: 'http://www.buenavistamancora.com',
-  // url_base_backend: 'http://localhost:8000/api',
-  // url_root_backend: 'http://localhost:8000',
-  // url_root_frontend: 'http://localhost:4200',
+  //url_base_backend: 'http://localhost:8000/api',
+  //url_root_backend: 'http://localhost:8000',
+  //url_root_frontend: 'http://localhost:4200',
 
   key_acceso_backend: 'Rdt6uToQ.10xQDsISuxkIHMGvs4ZOFRt18Is8GEta'
 
