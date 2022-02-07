04a386c3254ae3fab59b397ed47b8c5b61d7b6c6
wolfrit <wolfrit@gmail.com>
2022-02-07 06:27:33 +0000

    cambio 01:27 07/02/22

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  url_base_backend: 'http://reserva.buenavistamancora.com/api',
-  url_root_backend: 'http://reserva.buenavistamancora.com',
-  url_root_frontend: 'http://www.buenavistamancora.com',
+  url_base_backend: '//reserva.buenavistamancora.com/api',
+  url_root_backend: '//reserva.buenavistamancora.com',
+  url_root_frontend: '//www.buenavistamancora.com',
   //url_base_backend: 'http://localhost:8000/api',
   //url_root_backend: 'http://localhost:8000',
   //url_root_frontend: 'http://localhost:4200',
